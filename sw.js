809c8a035e24084e08abf57b9da26de0c3925f04
Brandup26 <anasgaber504@gmail.com>
2026-06-20 13:57:28 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,23 @@
-const CACHE_NAME = 'polovrita-v1';
+const CACHE_NAME = 'bolofreta-app-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    './images/llogo.jpg'
+    'index.html',
+    'manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+// تثبيت الملفات في الكاش
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request))
+// استدعاء الملفات بسرعة فائقة من الكاش
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
