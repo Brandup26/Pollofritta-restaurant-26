3880f83f818646f16deb4d9fc259bf1f00be4f4e
Brandup26 <anasgaber504@gmail.com>
2026-06-20 03:22:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,17 @@
 const CACHE_NAME = 'polovrita-v1';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './images/llogo.jpg'
+    './',
+    './index.html',
+    './manifest.json',
+    './images/llogo.jpg'
 ];
 
-// تثبيت الـ Service Worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// تفعيل وتحديث الكاش
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// استراتيجية جلب الملفات (Network First for data, Cache for assets)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
 });
