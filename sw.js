045ad32ace33c196e5cd2a82488a297e8453b057
Brandup26 <anasgaber504@gmail.com>
2026-06-20 03:00:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,17 @@
 const CACHE_NAME = 'polovrita-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+    './',
+    './index.html',
+    './manifest.json',
+    './images/llogo.jpg'
 ];
 
-// تثبيت التطبيق وتخزين الملفات
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// جلب البيانات من الكاش للسرعة الفائقة
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
 });
